dca41855a949091dcfd32002729f97b0c63f2c2a
Jed Geoghegan <jedgeoghegan@gmail.com>
2025-10-13 03:06:53 +0000

    Sync LeetCode submission Runtime - 11 ms (77.55%), Memory - 17.8 MB (100.00%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,5 +1,24 @@
 from collections import Counter
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        # edge cases:
+        # check if same length first
+        # brute: sort and if each letter is hte same, then anagram
+        # Hashmpa; compare frequencies
+        if len(s) != len(t):
+            return False
+
+        counter = {}
+
+        for char in s:
+            counter[char] = counter.get(char, 0) + 1
+        
+        for char in t:
+            if char not in counter or counter[char] == 0:
+                return False
+            counter[char] -= 1
+        
+        return True
+            
+       
+         
