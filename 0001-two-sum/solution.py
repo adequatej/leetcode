b3e50bb8f6e0f0b272f3aae993a95768eed310e7
Jed Geoghegan <jedgeoghegan@gmail.com>
2025-11-23 18:10:34 +0000

    Sync LeetCode submission Runtime - 1821 ms (8.71%), Memory - 18.4 MB (100.00%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,15 +1,26 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numMap = {}
-        n = len(nums)
+        # nums: list of nums
+        # target: int
+        # return: indices of two #'s such that add up to target 
 
-        # Build hash table
-        for i in range(n):
-            complement = target - nums[i]
-            if complement in numMap:
-                return [numMap[complement], i]
-            numMap[nums[i]] = i
-        
+        # brute force: iteratively go thorugh each pair and see fi they add up to target using nested loops, where otuer loop iterates first element to second to last element, and inner loop iterates next element to last element (O(n^2))
+
+        # to get the length of the array of ints 
+        n = len(nums)
+        # go through first 
+        for i in range(n - 1):
+            # second go thoruigh for second int 
+            for j in range(i + 1, n):
+            # if both add up to target, return two indices
+                if nums[i] + nums[j] == target:
+                    return [i,j]
         return []
+
+            
+
+
+ 
+            
 
         
