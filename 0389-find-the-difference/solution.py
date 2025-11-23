faa9be6da40b2c3d6407e5249ff0a838e46848ab
Jed Geoghegan <jedgeoghegan@gmail.com>
2025-11-23 00:02:15 +0000

    Sync LeetCode submission Runtime - 3 ms (51.56%), Memory - 17.8 MB (100.00%)

--- 0389-find-the-difference/solution.py
+++ 0389-find-the-difference/solution.py
@@ -1,8 +1,27 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        r = 0
-        for rs in s: 
-            r ^= ord(rs)
-        for rt in t:
-            r ^= ord(rt)
-        return chr(r)
+
+        # string t is made up of random shuffled letters from string s and one more letter at a random position 
+        # so just count the length of t and hten subtract each letter of s from string t and then just find the last letter to return the last letter that was added to t 
+        # use a hashmap to count each char in stirng t
+        # subtract -1 if same key from string s so that if numbers of count are 0, delete key/value from HashMap, to get leftover 
+
+        # initialize dict to store character counts
+        count = {}
+
+        # count chars in string t
+        for c in t:
+            count[c] = count.get(c, 0) + 1
+
+        # substract counts for chars in string s
+        for c in s:
+            count[c] -= 1
+            if count[c] == 0:
+                del count[c]
+        
+        # return remaining char in dict as the difference
+        return list(count.keys())[0]
+        
+
+
+        
