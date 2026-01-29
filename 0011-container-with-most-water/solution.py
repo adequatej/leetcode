c1cdd315f2862f430065f6fb62cc289384681dd1
Jed Geoghegan <jedgeoghegan@gmail.com>
2026-01-29 05:06:36 +0000

    Sync LeetCode submission Runtime - 66 ms (12.06%), Memory - 29.8 MB (14.71%)

--- 0011-container-with-most-water/solution.py
+++ 0011-container-with-most-water/solution.py
@@ -1,16 +1,24 @@
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        maxArea = 0
+    def maxArea(self, heights: List[int]) -> int:
+        # have to take into account not only height of each bar, but also distance from each other
+        # Brute force idea: try all pairs (i, j) of bars in the array, and calculate the water for each pair,
+        #                   returning the maximum water among all pairs but this would be O(n^2)
+        # Better way: Two Pointer -> Initialize two pointers, where at each step, 
+        #                            we calculate the amount of water using the formula (j - i) * min(heights[i], heights[j])
+        #                            Then we move the pointer that has the smaller height value since the amount of water depends 
+        #                            only on the minimum height  
 
-        while left < right:
-            currentArea = min(height[left], height[right]) * (right - left)
-            maxArea = max(maxArea, currentArea)
+        l, r = 0, len(heights) - 1
 
-            if height[left] < height[right]:
-                left += 1
+        result = 0
+        
+        while l < r:
+            area = min(heights[l], heights[r]) * (r - l)
+            result = max(result, area)
+            if heights[l] <= heights[r]:
+                l += 1
             else:
-                right-= 1
-                
-        return maxArea
+                r -= 1
+        return result            
+
+        
